6d4afa247d6da594119c748ea8ddab705c773aa1
zm711 <92116279+zm711@users.noreply.github.com>
2023-05-15 09:20:13 -0400

    Update utils.py
    add window util.

--- spikeanalysis/src/utils.py
+++ spikeanalysis/src/utils.py
@@ -1,5 +1,14 @@
 
 import json
+from typing import Union
+import numpy as np
+
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
 
 
 def jsonify_parameters(parameters:dict):
@@ -16,3 +25,34 @@ def jsonify_parameters(parameters:dict):
 
     with open('analysis_parameters.json') as write_file:
         json.dump(new_parameters)
+
+
+
+def verify_window_format(window: Union[list,list[list]], num_stim: int)->list[list]:
+
+    """Utility function for making sure window format is correct for analysis 
+    and plotting functions
+
+    Parameters
+    ----------
+    window: Union[list, list[list]]
+        Either a single sequence of (start, stop) or one list per stimulus
+        each containing their own (start, stop)
+    num_stim: int
+        The number of stimuli being analyzed
+        
+    Returns
+    -------
+    windows: list[list]
+        A list of lists with each stimulus having a sequence of start stop"""
+
+    if len(window) == 2 and isinstance(window[0], (int, float)):
+        windows = [window] * num_stim
+    else:
+        assert (
+            len(window) == num_stim
+        ), f"Please enter correct number of lists for stim \
+            bsl_window length is {len(window)} and should be {num_stim}"
+        windows= window
+
+    return windows
