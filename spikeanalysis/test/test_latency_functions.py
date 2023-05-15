4925607c34ecfe3233244d38eca5f00dff01ad78
zm711 <92116279+zm711@users.noreply.github.com>
2023-05-15 17:20:35 -0400

    Create test_latency_functions.py
    test the latency functions

---
+++ spikeanalysis/test/test_latency_functions.py
@@ -0,0 +1,34 @@
+from analysis_utils import latency_functions as lf
+
+import numpy as np
+
+def test_latency_latency_core():
+    test_array = np.array([0,0,0,1,1,0,0,1,0,0,1])
+    test_array = np.expand_dims(test_array,axis=0)
+    lat = lf.latency_core_stats(3, test_array, time_bin_size = 0.05)
+    print(lat)
+    assert lat == [0.5]
+
+def test_latency_core_no_lat():
+
+    test_array = np.array([0,0,1,0,0,0,1,0,0,0])
+    test_array = np.expand_dims(test_array, axis=0)
+    lat = lf.latency_core_stats(10, test_array, time_bin_size = 1)
+    print(lat)
+    assert lat == [9.]
+
+def test_latency_median():
+
+    test_array = np.array([0,0,1,0,0,0,0,0,])
+    test_array = np.expand_dims(test_array, axis=0)
+    lat = lf.latency_median(test_array, time_bin_size = 1)
+    print(lat)
+    assert lat == [3.]
+
+def test_latency_nan():
+
+    test_array = np.array([0,0,0,0,0,0,0,0,])
+    test_array = np.expand_dims(test_array, axis=0)
+    lat = lf.latency_median(test_array, time_bin_size = 1)
+    print(lat)
+    assert np.isnan(lat)
