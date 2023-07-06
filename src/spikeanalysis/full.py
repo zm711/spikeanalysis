22bf2c3a55dd38fc30de546d31d005d227c7fcbd
Zach McKenzie <92116279+zm711@users.noreply.github.com>
2023-07-06 16:05:32 -0400

    fix typo

--- src/spikeanalysis/full.py
+++ src/spikeanalysis/full.py
@@ -2,5 +2,5 @@ from .spike_data import SpikeData
 from .spike_analysis import SpikeAnalysis
 from .stimulus_data import StimulusData
 from .spike_plotter import SpikePlotter
-from. intrinsic_plotter import IntrinsicPlotter
+from .intrinsic_plotter import IntrinsicPlotter
 from .analog_analysis import AnalogAnalysis
