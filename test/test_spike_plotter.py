1b9ce71737c617872434a808b45c936f671e152e
zm711 <92116279+zm711@users.noreply.github.com>
2023-07-09 13:59:34 -0400

    test black formatting

--- test/test_spike_plotter.py
+++ test/test_spike_plotter.py
@@ -13,14 +13,14 @@ def test_SpikePlotter_kwargs():
 
     plotter = SpikePlotter(None, **{'dpi': 1200, 'x_axis': 'Time (ms)'})
     assert plotter.dpi == 1200
-    assert plotter.x_axis == 'Time (ms)'
-    assert plotter.figsize == (10,8)
+    assert plotter.x_axis == 'Time (ms)', 'check time is used'
+    assert plotter.figsize == (10,8), 'fig size should not be changed from default'
 
 
 def test_SpikePlotter_wrong_kwarg():
     with pytest.raises(AssertionError):
-        plotter = SpikePlotter(None, **{'x': 5})
+        plotter = SpikePlotter(None, **{'x': 5}), 'code should check for bad kwargs'
 
 def test_wrong_init():
     with pytest.raises(AssertionError):
-        plotter = SpikePlotter(1)
+        plotter = SpikePlotter(1), 'code should not accept arbitrary objects for plotting'
