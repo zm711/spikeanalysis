9df64539fb1fd2284d917867a95b28b315b70e7f
zm711 <92116279+zm711@users.noreply.github.com>
2023-07-11 18:35:26 -0400

    linting

--- test/test_import.py
+++ test/test_import.py
@@ -1,23 +1,22 @@
 def test_import_SpikeAnalysis():
-
     from spikeanalysis.spike_analysis import SpikeAnalysis
 
 
 def test_import_StimulusDatat():
-
     from spikeanalysis.stimulus_data import StimulusData
 
 
 def test_importSpikePlotter():
-
     from spikeanalysis.spike_plotter import SpikePlotter
 
 
 def test_importAnalogAnalysis():
     from spikeanalysis.analog_analysis import AnalogAnalysis
 
+
 def test_importIntrinsicPlotter():
     from spikeanalysis.intrinsic_plotter import IntrinsicPlotter
+
 
 def test_import_all():
     import spikeanalysis
