c6804cffc269e450b5862a88257a58d98dc1577b
zm711 <92116279+zm711@users.noreply.github.com>
2023-07-28 20:58:18 -0500

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2023, Zach McKenzie"
 author = "Zach McKenzie"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.5"
+release = "0.0.6"
 
 
 # -- General configuration ---------------------------------------------------
