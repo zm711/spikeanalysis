f33d16e03288555487fa78878ade09a76ba89e6e
Zach McKenzie <92116279+zm711@users.noreply.github.com>
2023-08-07 17:39:01 -0400

    [WIP] make cdf call jit-able

--- src/spikeanalysis/analysis_utils/latency_functions.py
+++ src/spikeanalysis/analysis_utils/latency_functions.py
@@ -1,11 +1,11 @@
 import numpy as np
 from numba import jit
-from scipy import stats
+#from scipy import stats
+import math
 
-
+"""
 def latency_core_stats(bsl_fr: float, firing_data: np.array, time_bin_size: float):
-    """idea modified from Chase and Young, 2007: PNAS
-    p_tn(>=n) = 1 - sum_m_n-1 ((rt)^m e^(-rt))/m!"""
+   
 
     latency = np.zeros((np.shape(firing_data)[0]))
     for trial in range(np.shape(firing_data)[0]):
@@ -23,7 +23,39 @@ def latency_core_stats(bsl_fr: float, firing_data: np.array, time_bin_size: floa
             latency[trial] = (n_bin + 1) * time_bin_size
 
     return latency
+"""
 
+@jit(nopython=True)
+def latency_core_stats(bsl_fr: float, firing_data = np.array, time_bin_size: float):
+     """idea modified from Chase and Young, 2007: PNAS
+    p_tn(>=n) = 1 - sum_m_n-1 ((rt)^m e^(-rt))/m!"""
+    latency = np.zeros((np.shape(firing_data)[0]))
+    for trial in range(np.shape(firing_data)[0]):
+        for n_bin in range(np.shape(firing_data)[1] - 1):
+            final_prob = 1 - poisson_cdf(
+                int(np.sum(firing_data[trial][: n_bin + 1]) - 1),
+                bsl_fr * ((n_bin + 1) * time_bin_size),
+            )
+            if final_prob <= 10e-6:
+                break
+
+    if n_bin == np.shape(firing_data)[1] - 2:  # need to go to second last bin
+            latency[trial] = np.nan
+        else:
+            latency[trial] = (n_bin + 1) * time_bin_size
+
+    return latency
+
+@jit(nopython=True)
+def poisson_pdf(k: int, mu: float):
+    return (k**x) / math.factorial(k) * math.exp(-mu)
+
+@jit(nopython=True)
+def poisson_cdf(k: int, mu: float):
+    value = 0.0
+    for k in range(k+1):
+        value += poisson_pdf(k, mu)
+    return value
 
 @jit(nopython=True)
 def latency_median(firing_counts: np.array, time_bin_size: float):  # pragma no cover
