3c539328d27d5f05c0890fd12bcc60b62f8f0314
zm711 <92116279+zm711@users.noreply.github.com>
2023-10-04 13:50:23 -0400

    abstract plotting functions

--- src/spikeanalysis/__init__.py
+++ src/spikeanalysis/__init__.py
@@ -6,6 +6,7 @@ from .intrinsic_plotter import IntrinsicPlotter
 from .analog_analysis import AnalogAnalysis
 from .curated_spike_analysis import CuratedSpikeAnalysis, read_responsive_neurons
 from .stats_functions import kolmo_smir_stats
+from .plotting_functions import plot_piechart
 
 import importlib.metadata
 
