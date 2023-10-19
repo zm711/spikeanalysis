3f0643a51afc84f3238b8d0860582f42da5cc5ff
zm711 <92116279+zm711@users.noreply.github.com>
2023-10-19 08:36:38 -0400

    only load the psths

--- src/spikeanalysis/merged_spike_analysis.py
+++ src/spikeanalysis/merged_spike_analysis.py
@@ -51,6 +51,8 @@ class MergedSpikeAnalysis:
         assert (
             len(self.spikeanalysis_list) >= 2
         ), f"merge should only be run on multiple datasets you currently have {len(self.spikeanalysis_list)} datasets"
+
+        assert isinstance(self.spikeanalysis_list[0].psths, dict), "must have psth to merge"
         cluster_ids = []
         for idx, sa in enumerate(self.spikeanalysis_list):
             if isinstance(self.name_list, list):
@@ -71,42 +73,13 @@ class MergedSpikeAnalysis:
 
         self.events = events
 
+        psths_list = []
         for idx, sa in enumerate(self.spikeanalysis_list):
-            z_score_list = []
-            try:
-                z_score_list.append(sa.z_scores)
+            psths_list.append(sa.psths)
 
-                z_bins = sa.z_bins
-                z_windows = sa.z_windows
-                have_z_data = True
+        data_merge = _merge(psths_list, stim_name)
 
-            except AttributeError:
-                if self.name_list is not None:
-                    print(f"no z score data for data set {self.name_list[idx]}")
-
-            fr_list = []
-            try:
-                fr_list.append(sa.fr__)
-
-            except AttributeError:
-                if self.name_list is not None:
-                    print(f"no raw firing rate data for data set {self.name_list[idx]}")
-                have_raw_data = False
-
-        if len(z_score_list) >= 2:
-            z_scores = _merge(z_score_list, stim_name=stim_name)
-            self.z_scores = z_scores
-            if stim_name is None:
-                self.z_bins = z_bins
-                self.z_windows = z_windows
-            else:
-                self.z_bins = {}
-                self.z_bins[stim_name] = z_bins[stim_name]
-                self.z_windows = {}
-                self.z_windows[stim_name] = z_windows[stim_name]
-
-        if len(fr_list) >= 2:
-            fr_scores = _merge(fr_list, stim_name=stim_name)
+        self.data = data_merge
 
     def _merge(dataset_list: list, stim_name: str):
         data_merge = {}
@@ -128,12 +101,8 @@ class MergedSpikeAnalysis:
     def get_merged_data(self):
         msa = MSA()
         msa.cluster_ids = self.cluster_ids
-        try:
-            msa.z_scores = self.z_scores
-            msa.z_bins = self.z_bins
-            msa.z_windows = self.z_windows
-        except AttributeError:
-            pass
+        msa.events = self.events
+        msa.psths = self.data
 
         return msa
 
@@ -144,23 +113,14 @@ class MSA(SpikeAnalysis):
     def get_raw_psth(self):
         raise NotImplementedError
 
-    def z_score_data(self):
-        raise NotImplementedError
-
-    def latencies(self):
-        raise NotImplementedError
-
     def set_spike_data(self):
         print("data is immutable")
 
     def set_stimulus_data(self):
         print("data is immutable")
 
-    def get_raw_firing_rate(self):
-        raise NotImplementedError
-
-    def trial_correlation(self):
-        raise NotImplementedError
-
     def get_interspike_intervals(self):
+        raise NotImplementedError
+
+    def compute_event_interspike_intervals(self, time_ms: float = 200):
         raise NotImplementedError
